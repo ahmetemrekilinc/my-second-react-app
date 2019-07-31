83725312f60977be5a38317905e2270c518d7d5c
ahmetemrekilinc <aekilinc@gmail.com>
2019-07-31 15:39:03 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. and my text
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent name="ahmet emre" >this is my children</MyComponent>
+        <MyComponent name="hasan" >this is your children</MyComponent>
     </div>
   );
 }

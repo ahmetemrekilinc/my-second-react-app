e8051e2a04f6eb1b938374278a3137af24c8d50e
ahmetemrekilinc <aekilinc@gmail.com>
2019-07-31 16:32:17 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyComponent from './components/MyComponent';
+import MyDataComponent from './components/MyDataComponent';
 
 function App() {
   return (
     <div className="App">
-      <MyComponent name="ahmet emre" >this is my children</MyComponent>
+        <MyComponent name="ahmet emre" >this is my children</MyComponent>
         <MyComponent name="hasan" >this is your children</MyComponent>
+        <MyDataComponent></MyDataComponent>
     </div>
   );
 }
